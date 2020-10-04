66d586d47acc45cd543c03e55b2b4dff334a67e5
Crowdbotics <team@crowdbotics.com>
2020-10-04 02:37:05 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dreaming_drip_21045/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dreaming_drip_21045;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "dreaming_drip_21045";
+    }
+}
